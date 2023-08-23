54c7f910bc9d3b723eef07abffba488f0f92146a
rajivhere <rajivhere@gmail.com>
2023-08-23 17:02:03 -0400

    startup msg udpated

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,7 +5,7 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async ()=>{
-  console.log("Staring payments service...");  
+  console.log("payments service starting...");  
 
 
 if(!process.env.JWT_KEY)
