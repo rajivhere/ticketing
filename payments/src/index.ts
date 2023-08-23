7732d45a1caf5866def9ad26dd35490e8a171142
rajivhere <rajivhere@gmail.com>
2023-08-23 16:44:12 -0400

    workflows updated with backslash

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,7 +5,7 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async ()=>{
-  console.log("Staring payments service.");  
+  console.log("Staring payments service...");  
 
 
 if(!process.env.JWT_KEY)
